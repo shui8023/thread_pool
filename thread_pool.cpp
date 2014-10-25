6506e274751e54d2bbd45e4da5a3b199087ee88f
shui8023 <dubingyangxiyou@gmail.com>
2014-10-25 23:14:01 +0800

    线程

---
+++ thread_pool.cpp
@@ -0,0 +1,39 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  thread_pool.cpp
+ *
+ *    Description:  实现三个类，一个任务池，一个线程池，一个管理任务，线程的类
+ *
+ *        Version:  1.0
+ *        Created:  2014年10月25日 23时03分19秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Shui(dubingyang), dubingyangxiyou@gmail.com
+ *        Company:  Class 1201 of software engineering
+ *
+ * =====================================================================================
+ */
+
+/*目标:实现10个进程等待，然后在任务池中取任务，当任务数多于10个时，线程会在产生相应的线程
+ *来执行，当没有任务时，会把其他的线程kill掉
+ *
+ */
+
+class task_pool
+{
+	
+};
+
+class pthread_pool
+{
+
+};
+
+class mangage_task_pthread
+{
+	
+};
+
+
